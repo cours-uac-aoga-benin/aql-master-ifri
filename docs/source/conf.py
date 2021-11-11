2a244ef00100b495fe991ce0b6804de7d8659f6d
John Aoga <johnaoga@gmail.com>
2021-11-11 11:15:55 +0100

    adding files to test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AQL'
+copyright = '2021, John Aoga'
+author = 'John Aoga'
 
 release = '0.1'
 version = '0.1.0'
